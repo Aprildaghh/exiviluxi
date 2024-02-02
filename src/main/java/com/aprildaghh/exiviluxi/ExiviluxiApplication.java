7b683d2f421e0b69af5207cd8ba4c7c065847df2
Abdullah Aslan <abdullah.aslan.se@gmail.com>
2024-02-02 05:22:37 +0300

    first commit

---
+++ src/main/java/com/aprildaghh/exiviluxi/ExiviluxiApplication.java
@@ -0,0 +1,13 @@
+package com.aprildaghh.exiviluxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExiviluxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExiviluxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprildaghh/exiviluxi/ExiviluxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprildaghh.exiviluxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExiviluxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
